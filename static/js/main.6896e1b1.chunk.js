12badc6aaf5e29e67963e562a05aacd07219f446
Pragya Mukherjee <pragyamukherjee2017@gmail.com>
2021-11-09 21:25:06 +0530

    Updates

---
+++ static/js/main.6896e1b1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbmi=this.webpackJsonpbmi||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(0),i=t.n(c),r=t(3),s=t.n(r),o=(t(8),t(9),t(1));var a=function(){return Object(o.jsx)("div",{className:"App",children:"hello"})};s.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.6896e1b1.chunk.js.map
